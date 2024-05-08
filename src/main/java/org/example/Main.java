b21721cfe235a966fc0ab458e44c0500617081bb
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-05-08 10:13:34 -0400

    Difference currencies

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,20 @@
 package org.example;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
+        Locale deLocale = new Locale("de", "De");
+        Locale usLocale = new Locale("en", "Us");
+        long number = 12345989898L;
+        NumberFormat denf = NumberFormat.getInstance(deLocale);
+        NumberFormat unsf = NumberFormat.getInstance(usLocale);
+
+        System.out.println(denf.format(number));
+        System.out.println(unsf.format(number));
     }
 }
